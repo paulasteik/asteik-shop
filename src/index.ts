9cf7a7fdc1f0d164ee3afc5a220fe5c9d2b13421
aurlin05 <aurlinmika05@gmail.com>
2025-04-08 17:28:21 +0000

    deploy

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,9 @@
 import { bootstrap, runMigrations } from '@vendure/core';
 import { config } from './vendure-config';
+import express from 'express';
+const app = express();
+
+app.set('trust proxy', true);
 
 runMigrations(config)
     .then(() => bootstrap(config))
